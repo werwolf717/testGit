49dbd888dbe7c241c9695a67a06281685014e4be
Sergey <s.o.balakin@gmail.com>
2022-10-26 14:30:38 +0300

    Init commit

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../lib/libhello.pdb"
+  "../lib/libhello.so"
+  "CMakeFiles/hello.dir/src/hello.cpp.o"
+  "CMakeFiles/hello.dir/src/hello.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/hello.h
@@ -0,0 +1,4 @@
+#ifndef HELLO_H__
+#define HELLO_H__
+extern void print_hello(void);
+#endif
